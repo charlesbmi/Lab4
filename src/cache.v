1ac02bc5c8aa2bc044c84f256b686f28c4f8a444
Charles Guan <charles.guan@stanford.edu>
2014-03-06 17:42:20 -0800

    added cache writing (memory writing not yet done)

--- src/cache.v
+++ src/cache.v
@@ -13,6 +13,12 @@ module cache (
     output wire complete
 );
 
+    wire [1:0] offset = addr[3:2];
+    wire [5:0] index = addr[9:4];
+    wire [21:0] tag = addr[31:10];
+
+    // if valid_bits[index] && (tag_bits[index] == tag), set that cache value
+
     // when implementing your cache FSM logic
     // you will write and read from these structures
     // containing the valid bits, tag, and data, for each cache
@@ -25,6 +31,22 @@ module cache (
     reg [63:0] valid_bits;
     reg [21:0] tag_bits [63:0];
     reg [127:0] data_blocks [63:0];
+
+    // write to cache on hit
+    wire hit = valid_bits[index] && (tag_bits[index] == tag);
+    always @(*) begin
+        if (we && hit) begin
+            data_blocks[index][((offset+1)<<5)-1 : offset << 5] = din;
+        end
+        /* If above doesn't work inside the if statement
+            case (offset)
+                2'b00: data_blocks[index] = {data_blocks[index][127:32], din};
+                2'b01: data_blocks[index] = {data_blocks[index][127:64], din, data_blocks[index][31:0]};
+                2'b10: data_blocks[index] = {data_blocks[index][127:96], din, data_blocks[index][64:0]};
+                2'b11: data_blocks[index] = {din, data_blocks[index][95:0]};
+            endcase
+        */
+    end
 
     // outputs from DRAM
     wire [127:0] dram_out;
@@ -55,13 +77,12 @@ module cache (
     wire dram_we = we;
     wire dram_re = re;
 
-    // address a whole block per word (2^4 bytes)
+    // address a whole block per word (2^4 bytes) TODO: If address is in bytes, shouldn't this be MEM_DEPTH+1:4? That's what may be changed, with the rest as offsetting.
     // change this in your implementation
     wire [`MEM_DEPTH-3:0] dram_addr = addr[`MEM_DEPTH-1:2];
 
     // only use the first word in the cache line
     // change this in your implementation
-    wire [127:0] dram_in = {96'd0, din};
     wire [31:0] cache_dout = dram_out[31:0];
 
     // the cache is done when DRAM is done
@@ -75,7 +96,7 @@ module cache (
                   .we(dram_we),
                   .re(dram_re),
                   .addr(dram_addr),
-                  .din(dram_in),
+                  .din(din),
                   .dout(dram_out),
                   .complete(dram_complete));
     
